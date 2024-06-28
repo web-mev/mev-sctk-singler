40f720e68c065dd8b42343baceefd4796e5cdbf8
Brian Lawney <brianlawney@hsph.harvard.edu>
2024-06-28 09:13:35 -0400

    Fix barcode names in output

--- docker/singler.R
+++ docker/singler.R
@@ -1,5 +1,6 @@
 suppressMessages(suppressWarnings(library("singleCellTK")))
 suppressMessages(suppressWarnings(library("Matrix")))
+suppressMessages(suppressWarnings(library("tidyverse")))
 suppressMessages(suppressWarnings(library("optparse")))
 
 # args from command line:
@@ -67,7 +68,21 @@ cnts <- read.table(
     file = opt$input_file,
     sep = "\t",
     row.names = 1,
-    header=T
+    header=T,
+    check.names=FALSE
+)
+
+# above, we set check.names=F to prevent the mangling of the sample names.
+# Now, we stash those original sample names and run make.names, so that any downstream
+# functions, etc. don't run into trouble. In the end, we convert back to the original names
+orig_cols = colnames(cnts)
+new_colnames = make.names(orig_cols)
+colnames(cnts) = new_colnames
+
+colname_mapping = data.frame(
+    orig_names = orig_cols,
+    row.names=new_colnames,
+    stringsAsFactors=F
 )
 
 # change to a sparse matrix representation, necessary for SCE
@@ -103,15 +118,18 @@ df.final <- data.frame(
     cell_types = as.vector(sce[[varName]])
 )
 
+m <- merge(df.final,colname_mapping, by.y=0, by.x='cell_barcodes')
+m <- column_to_rownames(m, var='orig_names')
+
 # Write results to file
 output_filename <- paste(opt$output_file_prefix, 
   'cell_types',
   opt$reference,
   opt$level, 'tsv', sep='.')
 write.table(
-    df.final,
+    m['cell_types'],
     output_filename,
     sep='\t', 
     quote=F, 
-    row.names = FALSE,
+    row.names = TRUE,
 )
